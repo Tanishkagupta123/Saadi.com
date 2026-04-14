fdc8f11955d32c1505577bed49cfc0619b0e5c5d
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-04-14 22:50:49 +0530

    first commit

--- src/Pages/Religion.jsx
+++ src/Pages/Religion.jsx
@@ -72,6 +72,7 @@ const Religion = () => {
           <option>Maithili</option>
           <option>Dogri</option>
 
+
         </select>
 
         <select onChange={(e) => setCountry(e.target.value)} className="border px-4 py-2 w-64 rounded">
