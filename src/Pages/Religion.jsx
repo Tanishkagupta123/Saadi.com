0bc61c5217f0413e0e923f5cd5b4597015768cc3
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-04-20 23:34:18 +0530

    first commit

--- src/Pages/Religion.jsx
+++ src/Pages/Religion.jsx
@@ -72,7 +72,7 @@ const Religion = () => {
           <option>Dogri</option>
 
         </select>
-
+        
 
         <select onChange={(e) => setCountry(e.target.value)} className="border px-4 py-2 w-64 rounded">
           <option value="">Select Country</option>
@@ -93,5 +93,4 @@ const Religion = () => {
   )
 }
 
-export default Religion
-;
+export default Religion;
